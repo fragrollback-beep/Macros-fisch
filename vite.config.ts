4d05478250389b6393f4316f95e8806ee81052bd
fragrollback-beep <fragrollback@gmail.com>
2026-05-17 16:59:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Macros-fisch/', // Добавили эту строчку, чтобы пути вели в твою подпапку
-})
+  base: '/Macros-fisch/', // Указываем правильный путь для GitHub Pages здесь
+  plugins: [react(), tailwindcss(), viteSingleFile()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
